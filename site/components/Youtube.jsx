ebdc4956b404b56d4d3e20b74342b3a5a701150a
Robert Mao <robmao@roberts-imac.lan>
2022-05-23 15:39:37 -0700

    adding youtube

---
+++ site/components/Youtube.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+
+function Youtube() {
+  return (
+    <div>
+      <YouTube videoId="K-J7qU1CNSo" />
+    </div>
+  );
+}
+
+export default Youtube;
