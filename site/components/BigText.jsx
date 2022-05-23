47d21338b1a0bc63c9415eaff391927441d9bc43
Robert Mao <robmao@roberts-imac.lan>
2022-05-23 04:00:17 -0700

    adding test component

--- site/components/BigText.jsx
+++ site/components/BigText.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import urlFileManager from 'less/lib/less-node/url-file-manager';
 
-function BigText({ tag, text }) {
+function BigText({ tag, text, size }) {
   return (
-    <div>
+    <div
+      flex=""
+      items="center"
+      style={{
+        color: `black`,
+        fontSize: `${size}`,
+        padding: `50px`,
+        fontFamily: `Calibre-R,sans-serif`,
+        letterSpacing: `-.04em`,
+        background: `url(https://cdn.coda.io/assets/8438f078d683/img/bg-tomato.jpg)`,
+      }}>
       <div>{tag}</div>
       <div>{text}</div>
     </div>
@@ -12,11 +24,13 @@ function BigText({ tag, text }) {
 BigText.propTypes = {
   tag: PropTypes.string,
   text: PropTypes.string,
+  size: PropTypes.string,
 };
 
 BigText.defaultProps = {
-  tag: 'hello',
+  tag: '',
   text: '',
+  size: '',
 };
 
 export default BigText;
