5e10d3cc64b763b915204d80b35bbcac35bd71bc
wangshijun <wangshijun2010@gmail.com>
2022-08-23 18:10:15 +0800

    chore: update navigation and polish twitter timeline

--- .blocklet-page/components/TwitterTimelineEmbed.jsx
+++ .blocklet-page/components/TwitterTimelineEmbed.jsx
@@ -1,3 +1,9 @@
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
-export default TwitterTimelineEmbed;
+export default (props) => {
+  return (
+    <div style={{ maxHeight: 400, overflowY: 'scroll' }}>
+      <TwitterTimelineEmbed {...props} />
+    </div>
+  );
+};
